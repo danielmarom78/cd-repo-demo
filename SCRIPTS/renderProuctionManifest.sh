bdfe95fa734e99b711b7eb27ab4267391436b761
Daniel Marom <daniel.marom.78@gmail.com>
2024-03-28 15:48:01 +0200

    111

--- SCRIPTS/renderProuctionManifest.sh
+++ SCRIPTS/renderProuctionManifest.sh
@@ -9,18 +9,16 @@ export APP=app1
 export SOURCE_GIT_REPO=https://github.com/danielmarom78/cd-repo-demo.git
 export SOURCE_GIT_BRANCH=main
 export SOURCE_GIT_BASE_PATH=Application/
-export SOURCE_TEMP_DIR=tmp/source
-# Ensure the path is correctly defined without redundancy
+export SOURCE_TEMP_DIR=/tmp/render/source
 export SOURCE_GIT_PATH=$SOURCE_GIT_BASE_PATH$PROJ/$APP/prod
 
 
-# export TARGET_GIT_REPO=https://github.com/danielmarom78/cd-repo-demo.git
-# export TARGET_GIT_BRANCH=production
-# export TARGET_GIT_BASE_PATH= 
-# export TARGET_TEMP_DIR=tmp/target
-# export TARGET_GIT_PATH=$SOURCE_GIT_BASE_PATH/$PROJ/$APP
+export TARGET_GIT_REPO=https://github.com/danielmarom78/cd-repo-demo.git
+export TARGET_GIT_BRANCH=Production
+export TARGET_GIT_BASE_PATH= 
+export TARGET_TEMP_DIR=/tmp/render/target
+export TARGET_GIT_PATH=$TARGET_GIT_BASE_PATH$PROJ/$APP
 
-echo $SOURCE_GIT_PATH
 
 rm -rf $SOURCE_TEMP_DIR
 mkdir -p $SOURCE_TEMP_DIR
@@ -28,7 +26,15 @@ cd $SOURCE_TEMP_DIR
 git init
 git remote add origin $SOURCE_GIT_REPO
 git config core.sparseCheckout true
-echo $SOURCE_GIT_PATH >> .git/info/sparse-checkout
+echo $SOURCE_GIT_BASE_PATH >> .git/info/sparse-checkout
 git pull origin $SOURCE_GIT_BRANCH
 
 
+rm -rf $TARGET_TEMP_DIR
+mkdir -p $TARGET_TEMP_DIR
+cd $TARGET_TEMP_DIR
+git init
+git remote add origin $TARGET_GIT_REPO
+git config core.sparseCheckout true
+echo $TARGET_GIT_PATH >> .git/info/sparse-checkout
+git pull origin $TARGET_GIT_BRANCH
