9e84537bb724296adb26266bb8f5077f3a822a55
Daniel Marom <daniel.marom.78@gmail.com>
2024-03-28 15:22:44 +0200

    scripts

---
+++ SCRIPTS/renderProuctionManifest.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export PROJ=proj1
+export APP=app1
+
+export SOURCE_GIT_REPO=https://github.com/danielmarom78/cd-repo-demo.git
+export SOURCE_GIT_BRANCH=main
+export SOURCE_GIT_BASE_PATH=Application/
+export SOURCE_TEMP_DIR=tmp/source
+export SOURCE_GIT_PATH=Application/$SOURCE_GIT_BASE_PATH/$PROJ/$APP/prod
+
+export TARGET_GIT_REPO=https://github.com/danielmarom78/cd-repo-demo.git
+export SOURCE_GIT_BRANCH=production
+export TARGET_GIT_BASE_PATH= 
+export TARGET_TEMP_DIR=tmp/target
+export TARGET_GIT_PATH=$SOURCE_GIT_BASE_PATH/$PROJ/$APP
+
+rm -rf $SOURCE_TEMP_DIR
+mkdir -p $SOURCE_TEMP_DIR
+cd $SOURCE_TEMP_DIR
+rm * -f
+git init
+git remote add origin $SOURCE_GIT_REPO
+git config core.sparseCheckout true
+echo $SOURCE_GIT_PATH >> .git/info/sparse-checkout
+git pull origin $SOURCE_GIT_BRANCH
+
+
